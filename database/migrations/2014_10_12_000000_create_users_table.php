dc0e5188b7c748153af19f96ef9e8180602e5300
bleshbanz <bleshbanz@gmail.com>
2020-05-08 12:50:11 +0530

    user table migration update and Model/User.php moved, RegisterController user model update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->enum('role', array('user', 'redac', 'admin'));
+            $table->boolean('valid')->default(false);
         });
     }
 

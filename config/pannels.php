6b333d4d9e99ed90b27d40f324dbad3704d390d2
bleshbanz <bleshbanz@gmail.com>
2020-05-09 17:34:31 +0530

    add some config file for backend admin panel

---
+++ config/pannels.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    [
+        'color' => 'green',
+        'icon' => 'user',
+        'model' => \App\Models\User::class,
+        'name' => 'admin.new-registers',
+        'url' => 'admin/users?new=on',
+    ]
+];

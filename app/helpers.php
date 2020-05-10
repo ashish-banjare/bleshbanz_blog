9690699654340ec9b677a8fb062a76a2dc3d9970
bleshbanz <bleshbanz@gmail.com>
2020-05-10 12:14:32 +0530

    User Seeder

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,32 @@ if (!function_exists('currentRouteBootstrap')) {
         return request()->url() == route($route) ? ' class=active' : '';
     }
 }
+
+if (! function_exists('str_random')) {
+    /**
+     * Generate a more truly "random" alpha-numeric string.
+     *
+     * @param  int  $length
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    function str_random($length = 16)
+    {
+        return Str::random($length);
+    }
+}
+
+if (! function_exists('bcrypt')) {
+    /**
+     * Hash the given value.
+     *
+     * @param  string  $value
+     * @param  array   $options
+     * @return string
+     */
+    function bcrypt($value, $options = [])
+    {
+        return app('hash')->make($value, $options);
+    }
+}
